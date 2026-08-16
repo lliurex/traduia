c742bf0a38768146f5c362eb51948dc84b797806
M.Angel Juan <m.angel.juan@gmail.com>
2026-08-16 10:10:25 +0200

    repackage zero logic into traduia-config as standalone command

--- manage_i18n.sh
+++ manage_i18n.sh
@@ -12,7 +12,7 @@ cd "$(dirname "$0")"
 
 # Source files
 PYTHON_SRC="traduia_server.py check-traduia-server traduia-verify-models"
-BASH_SRC="traduia install-models-traduia traduia-make-repo traduia-open-url zero-lliurex-traduia.install-files/usr/share/zero-lliurex-traduia/traduia_script"
+BASH_SRC="traduia install-models-traduia traduia-make-repo traduia-open-url zero-lliurex-traduia.install-files/usr/sbin/traduia-config"
 
 # Languages to support
 LANGS=("es" "ca" "en")
